78a655cc5a6137806c4e0050bfcbb3482b4cf56a
Nemesty <frederic.duval@minhbo.fr>
2025-02-23 18:26:39 +0100

    Final commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#Band Name Generator by Nemesty
+
+print("Bienvenue le générateur de nom de groupe 2000 !\n")
+
+city = input("Quelle est le nom de la ville/village dans lequelle vous avez grandi ?\n")
+pet = input("Quel est le nom de votre animal de compagnie ?\n")
+
+band_name = city + " " + pet
+
+print(f"Voici un nom de groupe qui déchire : {band_name} !")
